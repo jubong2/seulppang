598d183aff118904824e705471cd380cbb72cc04
jubong <twoweekyear2@gmail.com>
2024-10-28 17:21:57 +0900

    로그인페이지, 회원가입페이지 작업완료, 메인페이지에 연결 완료, pet영역 배경 변경  inner영역 넓이 조정

--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,86 @@
 window.addEventListener("load", function () {
+  var besttSwiper = new Swiper(".veganSwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    slidesPerView: 1.5,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      1200: { slidesPerView: 2.5 },
+      800: { slidesPerView: 2 },
+      600: { slidesPerView: 2, spaceBetween: 20 },
+    },
+    on: {
+      loop: true,
+    },
+  });
+  var besttSwiper = new Swiper(".dessertSwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    slidesPerView: 1.5,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      1200: { slidesPerView: 2.5 },
+      800: { slidesPerView: 2 },
+      600: { slidesPerView: 2, spaceBetween: 20 },
+    },
+    on: {
+      loop: true,
+    },
+  });
+  var besttSwiper = new Swiper(".mealSwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    slidesPerView: 1.5,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      1200: { slidesPerView: 2.5 },
+      800: { slidesPerView: 2 },
+      600: { slidesPerView: 2, spaceBetween: 20 },
+    },
+    on: {
+      loop: true,
+    },
+  });
+
+  var besttSwiper = new Swiper(".mealSwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    slidesPerView: 1.5,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      1200: { slidesPerView: 2.5 },
+      800: { slidesPerView: 2 },
+      600: { slidesPerView: 2, spaceBetween: 20 },
+    },
+    on: {
+      loop: true,
+    },
+  });
+
   // 티겟 스와이퍼
   var targetSwiper = new Swiper(".targetSwiper", {
     pagination: {
